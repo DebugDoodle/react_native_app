4ad71c19f9884ee109621283f63513fac02430e0
DebugDoodle <149089448+DebugDoodle@users.noreply.github.com>
2023-11-30 12:31:23 +0530

    updated the UI

--- app/SecondScreen.js
+++ app/SecondScreen.js
@@ -1,35 +1,56 @@
-import React, { useEffect, useRef } from 'react';
-import { View, StyleSheet, Animated, Easing } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 import { Circle, G, Svg } from 'react-native-svg';
 
-const SecondScreen = () => {
-  const percentage = 75;
-  const progress = useRef(new Animated.Value(0)).current;
 
-  useEffect(() => {
-    Animated.timing(progress, {
-      toValue: percentage / 100,
-      duration: 1500,
-      easing: Easing.ease,
-      useNativeDriver: true,     
-    }).start();
-  }, [percentage, progress]);
+
+const SecondScreen = () => {
+  const radius = 30; // smaller radius
+  const strokeWidth = 8; // width of the progress bar
+  const circumference = 2 * Math.PI * radius;
+  const percentage = 25;
+  const progress = circumference - (percentage / 100) * circumference;
+
+  const currentHours = new Date().getHours();
+  let greetingMessage;
+  
+  // Set the greeting based on the current time
+  if (currentHours >= 5 && currentHours < 12) {
+    greetingMessage = 'Good morning';
+  } else if (currentHours >= 12 && currentHours < 18) {
+    greetingMessage = 'Good afternoon';
+  } else {
+    greetingMessage = 'Good evening';
+  }
 
   return (
     <View style={styles.container}>
-      <Svg width={150} height={150}>
-        <G rotation="-90" origin="75,75">
+      <Text style={styles.greetingText}>{greetingMessage}</Text>
+      <Svg height="100%" width="100%" viewBox="0 0 120 120">
+        <G rotation={"270"} origin="60,60">
+          {/* Background Circle */}
           <Circle
-            cx="75"
-            cy="75"
-            r="70"
-            fill="transparent"
+            cx="60"
+            cy="60"
+            r="30"
             stroke="white"
-            strokeWidth="10"
+            strokeWidth={strokeWidth}
+            fill="transparent"
           />
-          
+          {/* Progress Circle */}
+          <Circle
+            cx="60"
+            cy="60"
+            r="30"
+            stroke="black"
+            strokeWidth={strokeWidth}
+            fill="transparent"
+            strokeDasharray={circumference}
+            strokeDashoffset={progress}
+          />
         </G>
       </Svg>
+      <Text style={styles.percentageText}>{percentage}%</Text>
     </View>
   );
 };
@@ -37,9 +58,19 @@ const SecondScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
-    justifyContent: 'center',
     alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'black',
+  },
+   greetingText: {
+    fontSize: 24,
+    color: 'white',
+    
+  },
+  percentageText: {
+    position: 'absolute',
+    fontSize: 50,
+    color: 'white',
   },
 });
 
